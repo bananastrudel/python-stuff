11ea275c4819d16d6d03fc643f017032c61e3e97
bananastrudel <annieelizabethstruck@gmail.com>
2025-09-05 06:57:42 -0400

    Update cool_circle.py

--- cool_circle.py
+++ cool_circle.py
@@ -2,11 +2,11 @@ import turtle
 import random
 screen = turtle.Screen()
 turtle.speed(0)
-turtle.width(5)
+turtle.width(3)
 turtle.colormode(255)
 
-for p in range (44):
-    turtle.pencolor(random.randint(25,200),random.randint(10,20),random.randint(50,255))
+for p in range (23):
+    turtle.pencolor(random.randint(60,255),random.randint(10,20),random.randint(45,255))
     turtle.rt(125)
     for s in range (36):
         turtle.lt(10)
